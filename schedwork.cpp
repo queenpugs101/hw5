dd3b9ba09a51c94d0ff98f96a87caa023ea2c4c1
Amber Rawlins <abrawlin@usc.edu>
2025-12-11 06:57:20 +0000

    hw5 completed

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,10 +21,31 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool schedBuilder(  const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, vector<int>& shiftsWorked, 
+    size_t currentDay, size_t currentWorker);
 
 
 // Add your implementation of schedule() and other helper functions here
-
+/**
+ * @brief Produces a work schedule given worker availability,
+ *        the number of needed workers per day, and the maximum 
+ *        shifts any single worker is allowed. Returns true if
+ *        and the valid schedule if a solution exists, and false
+ *        otherwise. 
+ * 
+ * @param [in]  avail n x k vector of vectors (i.e. matrix) of the availability
+ *                    of the k workers for each of the n days
+ * @param [in]  dailyNeed Number of workers needed per day (aka d)
+ * @param [in]  maxShifts Maximum shifts any worker is allowed over 
+ *                        the n day period (aka m)
+ * @param [out] sched n x d vector of vectors indicating the d workers
+ *                    who are scheduled to work on each of the n days
+ * @return true if a solution exists; sched contains the solution
+ * @return false if no solution exists; sched is undefined (can be anything)
+ */
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -37,9 +58,58 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    // is matirx filled if yes return true
+    size_t n = avail.size(); //days
+    size_t k = avail[0].size(); //total workers
+    vector<int> shiftsWorked(k,0);
+    sched.assign(n, vector<Worker_T>(dailyNeed, INVALID_ID));
+    return schedBuilder(avail, dailyNeed, maxShifts, sched, shiftsWorked, 0, 0);
+    
 
 
 
 
 }
+bool schedBuilder(  const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, vector<int>& shiftsWorked, 
+    size_t currentDay, size_t currentWorker){
+    size_t n = avail.size(); // total days
+    size_t k = avail[0].size(); //total workers
+    if(n == currentDay){
+        return true;
+    }
+    if(currentWorker== dailyNeed){
+        return schedBuilder(  avail, dailyNeed, maxShifts, sched,  shiftsWorked, currentDay+1, 0);
+    }
+    //add workers 
+    for(size_t i = 0; i < k; i++){
+        if(avail[currentDay][i] == 1){
+        if(shiftsWorked[i] < maxShifts){
+            bool usedAlready = false;
+            for (size_t x = 0; x < currentWorker; x++) {
+                if (sched[currentDay][x] == i) {
+                    usedAlready = true;
+                    break;
+                }
+            }
+            if(!usedAlready){
+                    sched[currentDay][currentWorker] = i;
+                    shiftsWorked[i]++;
+                    if( schedBuilder(  avail, dailyNeed, maxShifts, sched,  shiftsWorked, currentDay, currentWorker+1)){
+                        return true;
+                    }
+                    shiftsWorked[i]--;
+                    sched[currentDay][currentWorker] = INVALID_ID;
+            }
+            }
+            }
+        }
+        
+    
+    return false; 
+
+}
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+bool inDictionary(const std::set<std::string>& dict, std::string& word);
+bool hasFloating(const std::string& floating, char letter);
+void formWord(const std::string& in, const std::string& floating,const std::set<std::string>& dict, std::string& word, unsigned int index, std::set<std::string>& validWords);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,54 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> validWords;
+    std::string word;
+    formWord(in, floating, dict, word, 0, validWords);
+    return validWords;
 
 }
 
 // Define any helper functions here
+
+//check if in dictionary 
+bool inDictionary(const std::set<std::string>& dict, std::string& word){
+    //std::set<std::string>::iterator it =dict.end() ;
+    return dict.find(word) != dict.end();
+}
+// returns true if floating is present 
+// bool hasFloating(const std::string& floating, char letter){
+//     if(floating.find(letter) != std::string::npos){
+//         return true;
+//     }
+//     return false;
+// }
+
+// add letters 
+void formWord(const std::string& in, const std::string& floating,const std::set<std::string>& dict, std::string& word, unsigned int index, std::set<std::string>& validWords){
+    if(in.length()-index < floating.length()){
+        return;
+    }
+    if(index == in.length()){
+        if(floating.length() == 0 && inDictionary(dict, word) ){
+            validWords.insert(word);
+        }
+        return;
+    }
+    if(in[index] != '-'){
+      word.push_back(in[index]);
+      formWord(in, floating, dict ,word, index+1, validWords);
+      word.pop_back();
+    }
+    else {
+         for( char l = 'a'; l<='z'; ++l) {
+            word.push_back(l);
+            std::string temp = floating;
+            if(temp.find(l) != std::string::npos){
+                temp.erase(temp.find(l), 1);  
+            }
+            formWord(in, temp, dict ,word, index+1, validWords);
+            word.pop_back();
+        }
+    }
+    
+}
